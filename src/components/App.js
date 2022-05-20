31fd28557c1fda8d17f285388eafeaf1fcd5a97e
demetrx <mcbright7447@gmail.com>
2022-05-20 19:04:01 +0300

    basic config

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import user from '../data/user.json';
+
+import Profile from './Profile/Profile';
+
+export const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
